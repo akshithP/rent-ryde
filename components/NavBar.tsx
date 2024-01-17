35358a0a9589cad40782911246ab5b4e0af67bc9
Akshith-Patil <akshithp13@gmail.com>
2024-01-17 22:39:16 +1100

    Created NavBar component with basic layout

---
+++ components/NavBar.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import CarLogo from "../public/icons/car-logo.svg";
+import { Links } from "@/types";
+
+// List containing all the link objects
+const linksList: Links[] = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Cars",
+    url: "/cars",
+  },
+  {
+    id: 1,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 1,
+    title: "Contact",
+    url: "/contact",
+  },
+];
+
+const NavBar = () => {
+  return (
+    <nav className="flex gap-4 text-textPrimary">
+      <div id="logo">
+        <Link href="/">
+          <Image
+            width={40}
+            height={32}
+            src={CarLogo}
+            alt="Rent Ryde Car Logo"
+          />
+        </Link>
+      </div>
+      <div id="navBarOptions" className="flex gap-4">
+        {linksList.map((link) => (
+          <Link key={link.id} href={link.url} className="hover:bg-primary">
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ types.ts
@@ -0,0 +1,5 @@
+export type Links = {
+    id: number;
+    title: string;
+    url: string;
+  };
