a53acb08ed7f5202531e38e31bd78b1667162669
Akshith-Patil <akshithp13@gmail.com>
2024-01-20 12:01:26 +1100

    Made the navbar responsive

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -35,9 +35,12 @@ const NavBar = () => {
   const [menuOpen, setMenuClose] = useState(false);
 
   return (
-    <nav className="flex justify-between items-center max-w-7xl mx-auto px-4 py-2 nav border-b border-primary text-textPrimary">
-      {/* Section for the logo icon and text */}
-      <div id="logo" className=" ">
+    <header className="border-b border-primary py-2">
+      <div
+        id="logo"
+        className="flex items-center justify-between  lg:max-w-7xl lg:mx-auto max-w-full px-8 flex-wrap w-full"
+      >
+        {/* Section for the logo icon and text */}
         <Link href="/" className="flex gap-2 mt-2 ">
           <Image
             width={38}
@@ -47,37 +50,37 @@ const NavBar = () => {
           />
           <h1 className="text-primary font-extrabold text-2xl">RENT RYDE</h1>
         </Link>
+
+        {/* Hamburger menu button, appears if the screen width less than md*/}
+        <button
+          className="md:hidden block"
+          onClick={() => setMenuClose(!menuOpen)}
+        >
+          <IoMenu className="text-4xl text-textPrimary hover:text-primary"></IoMenu>
+        </button>
+
+        {/* Navbar menu links, displayed if screen size greater than md other hidden under hamburger less than md*/}
+        <nav
+          className={` ${
+            menuOpen ? "block" : "hidden"
+          } w-full md:flex md:items-center md:w-auto`}
+        >
+          <ul className="md:flex md:justify-between text-base font-semibold text-textPrimary">
+            {linksList.map((link) => (
+              <li key={link.id}>
+                <Link
+                  key={link.id}
+                  href={link.url}
+                  className={`hover:bg-primary p-2 rounded transition-colors text-xl block px-5 py-2`}
+                >
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
-
-      {/* Hamburger menu button, appears if the screen width less than md*/}
-      <button
-        className="md:hidden block"
-        onClick={() => setMenuClose(!menuOpen)}
-      >
-        <IoMenu className="text-4xl hover:text-primary"></IoMenu>
-      </button>
-
-      {/* Navbar menu links, displayed if screen size greater than md other hidden under hamburger less than md*/}
-      <nav
-        className={`${
-          menuOpen ? "block" : "hidden"
-        } md:flex md:items-center font-semibold`}
-      >
-        <ul className="md:flex md:justify-between gap-7">
-          {linksList.map((link) => (
-            <li key={link.id}>
-              <Link
-                key={link.id}
-                href={link.url}
-                className={`hover:bg-primary p-2 rounded transition-colors text-xl`}
-              >
-                {link.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </nav>
+    </header>
   );
 };
 
