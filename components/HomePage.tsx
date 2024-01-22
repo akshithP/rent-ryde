0f54951265f88a9090a8ccbd96f622ce342f02b1
Akshith-Patil <akshithp13@gmail.com>
2024-01-22 22:22:54 +1100

    Implemented hero section, made it responsive

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React from "react";
-import { poppins } from "@/fonts";
 import { FaChevronRight } from "react-icons/fa6";
+import CarImage from "../public/car-map.png";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <div className="grid md:grid-cols-2 grid-cols-1">
+    <div className="grid md:grid-cols-2 grid-cols-1 gap-5 items-center lg:max-w-7xl lg:mx-auto max-w-full h-100">
       <div id="HeroText" className="grid gap-y-5 grid-cols-1">
-        <section className="font-extrabold text-6xl font-poppins text-textPrimary">
+        <section className="font-extrabold md:text-6xl text-5xl font-poppins text-textPrimary">
           <h1>
             WHERE TO NEXT? <br /> YOUR{" "}
             <span className="text-primary2">RYDE</span> AWAITS
@@ -21,13 +22,21 @@ const HomePage = () => {
           </p>
         </section>
         <section>
-          <button className="flex gap-2 justify-center items-center bg-primary2 text-textPrimary font-bold px-4 py-3 rounded hover:bg-black transition-colors">
+          <button className="flex gap-2 justify-center items-center bg-primary2 text-textPrimary font-bold px-4 py-3 rounded hover:bg-black transition-colors hover:scale-105 transition-all'">
             <p>Book Now</p>
             <FaChevronRight className="text-xl font-semibold" />
           </button>
         </section>
       </div>
-      <div id="CarImage">Image</div>
+      <div id="CarImage">
+        <Image
+          // width={500}
+          // height={500}
+          src={CarImage}
+          alt="Muscle Car with World Map Background"
+          className="w-full object-cover align-middle"
+        />
+      </div>
     </div>
   );
 };
