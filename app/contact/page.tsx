26d807c93303acac480d4d09e21529c7a7759e55
Akshith-Patil <akshithp13@gmail.com>
2024-01-24 09:52:54 +1100

    Created the left side of the form, containing contact information

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,104 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import { BiSolidPhoneCall as Phone } from "react-icons/bi";
+import { IoMail as Mail } from "react-icons/io5";
+import { FaLocationDot as Pin } from "react-icons/fa6";
+import { FaInstagram as Instagram } from "react-icons/fa";
+import { FaFacebookSquare as Facebook } from "react-icons/fa";
+import { FaXTwitter as X } from "react-icons/fa6";
+import DualCircle from "../../public/dual-circles.png";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div id="pageContainer">
+      {/*-----------------TITLE----------------*/}
+      <div className="flex justify-center p-4">
+        <h1
+          id="title"
+          className="font-extrabold md:text-3xl text-2xl text-primary"
+        >
+          Contact Us
+        </h1>
+      </div>
+
+      {/*-----------------CONTACT FORM----------------*/}
+      <div
+        id="content"
+        className="grid grid-cols-5 gap-0 p-10 lg:max-w-7xl lg:mx-auto max-w-full"
+      >
+        {/*---------LEFT FORM----------*/}
+        <div className="col-span-2 bg-primary rounded-lg p-10 flex flex-col gap-y-20 font-poppins relative z-1">
+          <section className="gap-10">
+            <h1 className="text-2xl font-bold text-secondary2">
+              Contact Information
+            </h1>
+
+            <p className="text-lg font-normal text-textPrimary">
+              Any questions? Just send us a message!
+            </p>
+          </section>
+
+          {/*---------PHONE----------*/}
+          <a
+            href={`tel:+61-0234-5678`}
+            className="flex gap-5 justify-start items-center text-secondary2"
+          >
+            <Phone size={34} />
+            <p className="text-lg font-medium hover:underline">+61-0234-5678</p>
+          </a>
+
+          {/* <section className="flex gap-5 justify-start items-center text-secondary2">
+            <Phone size={34} />
+            <a
+              href={`tel:+61-0234-5678`}
+              className="text-lg font-medium hover:underline"
+            >
+              +61-0234-5678
+            </a>
+          </section> */}
+
+          {/*---------MAIL----------*/}
+          <section className="flex gap-5 justify-start items-center text-secondary2">
+            <Mail size={34} />
+            <a
+              href={`mailto:rentryde@help.com.au`}
+              className="text-lg font-medium hover:underline"
+            >
+              rentryde@help.com.au
+            </a>
+          </section>
+
+          {/*---------ADDRESS----------*/}
+          <section className="flex gap-5 justify-start items-center text-secondary2 z-10">
+            <Pin size={34} />
+            <a
+              href={`https://www.google.com/maps/search/?api=1&query=132+Kaer+Morhen+Street,+Melbourne,+3000+Australia`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-lg font-medium underline hover:text-textPrimary transition-colors"
+            >
+              132 Kaer Morhen Street, Melbourne, 3000 Australia
+            </a>
+          </section>
+
+          {/*---------ICONS----------*/}
+          <section className="flex gap-5 z-10">
+            <Facebook size={25} className="text-[#4267B2]" />
+            <Instagram size={25} className="text-[#E1306C]" />
+            <X size={25} />
+          </section>
+
+          <div className="absolute bottom-0 right-0 ">
+            <Image src={DualCircle} alt="Dual Circle" />
+          </div>
+        </div>
+
+        {/*---------RIGHT FORM----------*/}
+        <div className="col-span-3 bg-secondary2 rounded-lg z-0">RIGHT</div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
