a77a67e0324ddece4c63b9fdf3a49050aa256d26
Akshith-Patil <akshithp13@gmail.com>
2024-01-24 12:23:41 +1100

    Fixed issue with mobile view col spanning

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -4,7 +4,10 @@ import { IoMdSend as Send } from "react-icons/io";
 
 const ContactForm = () => {
   return (
-    <div id="mainContactForm" className="grid grid-cols-2 p-10 gap-14">
+    <div
+      id="mainContactForm"
+      className="grid md:grid-cols-2 grid-cols-1 p-10 md:gap-10 gap-5"
+    >
       {/*-----------------FIRST NAME----------------*/}
       <div id="firstName" className="flex flex-col gap-2">
         <p className="text-lg font-normal text-primary">First Name</p>
@@ -12,7 +15,7 @@ const ContactForm = () => {
           type="text"
           id="firstName"
           placeholder="Enter your first name..."
-          className="p-3 bg-transparent outline-none border-b-4 border-borderCol text-lg font-normal text-textPrimary placeholder:italic placeholder:text-base placeholder:font-light"
+          className="p-3 bg-transparent outline-none border-b-2 border-borderCol text-lg font-normal text-textPrimary placeholder:italic placeholder:text-base placeholder:font-light"
         ></input>
       </div>
 
@@ -23,7 +26,7 @@ const ContactForm = () => {
           type="text"
           id="lastName"
           placeholder="Enter your last name..."
-          className="p-3 bg-transparent outline-none border-b-4 border-borderCol text-lg font-normal text-textPrimary placeholder:italic placeholder:text-base placeholder:font-light"
+          className="p-3 bg-transparent outline-none border-b-2 border-borderCol text-lg font-normal text-textPrimary placeholder:italic placeholder:text-base placeholder:font-light"
         ></input>
       </div>
 
@@ -34,7 +37,7 @@ const ContactForm = () => {
           type="email"
           id="email"
           placeholder="Enter your email..."
-          className="p-3 bg-transparent outline-none border-b-4 border-borderCol text-lg font-normal text-textPrimary placeholder:italic placeholder:text-base placeholder:font-light"
+          className="p-3 bg-transparent outline-none border-b-2 border-borderCol text-lg font-normal text-textPrimary placeholder:italic placeholder:text-base placeholder:font-light"
         ></input>
       </div>
 
@@ -45,23 +48,23 @@ const ContactForm = () => {
           type="tel"
           id="phone"
           placeholder="Enter your number..."
-          className="p-3 bg-transparent outline-none border-b-4 border-borderCol text-lg font-normal text-textPrimary placeholder:italic placeholder:text-base placeholder:font-light"
+          className="p-3 bg-transparent outline-none border-b-2 border-borderCol text-lg font-normal text-textPrimary placeholder:italic placeholder:text-base placeholder:font-light"
         ></input>
       </div>
 
       {/*------------------------------------WRITE MESSAGE----------------------------------*/}
-      <div className="flex flex-col gap-2 col-span-2">
+      <div className="flex flex-col gap-2 md:col-span-2">
         <p className="text-lg font-normal text-primary">Message</p>
         <input
           type="text"
           id="message"
           placeholder="Write your message here..."
-          className="p-3 bg-transparent outline-none border-b-4 border-borderCol text-lg font-normal text-textPrimary placeholder:italic placeholder:text-base placeholder:font-light"
+          className="p-3 bg-transparent outline-none border-b-2 border-borderCol text-lg font-normal text-textPrimary placeholder:italic placeholder:text-base placeholder:font-light"
         ></input>
       </div>
 
       {/*------------------------------------SEND BUTTON----------------------------------*/}
-      <div className="flex flex-col gap-2 col-start-2 col-span-1">
+      <div className="flex flex-col gap-2 md:col-start-2 md:col-span-1">
         <button className="flex gap-2 mt-5 justify-center items-center bg-primary text-secondary2 font-bold px-4 py-3 rounded hover:bg-black hover:text-textPrimary transition-colors hover:scale-105 transition-all'">
           <p>Send Message</p>
           <Send className="text-xl font-semibold" />
