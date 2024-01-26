57d8b80fb55d58f8c7ab18f98df37b3555902ae4
Akshith-Patil <akshithp13@gmail.com>
2024-01-26 21:56:03 +1100

    Added api to connect and fetch cars data from DB

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-const Cars = () => {
-  return (
-    <div>Cars</div>
-  )
+async function getData() {
+  const response = await fetch("http://localhost:3000/api/cars", {
+    cache: "no-store",
+  });
+  if (!response.ok) return notFound();
+  return response.json();
 }
 
-export default Cars
+const Cars = () => {
+  // const data = await getData();
+  // console.log("PRINTING DATA FROM CAR PAGE");
+  // console.log(data);
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/cars")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  console.log("PRINTING DATA FROM CAR PAGE");
+  console.log(data);
+
+  return (
+    <div className="grid grid-cols-5 p-10">
+      {data &&
+        data?.map((car: any) => (
+          <div key={car?._id} className="text-textPrimary font-semibold text-lg">
+            <p>Brand: {car?.brand}</p>
+            <p>Model: {car?.model}</p>
+            <p>Year: {car?.year}</p>
+            <p>Km(s) per 1L: {car?.fuel_economy}km</p>
+            <Image
+              width={300}
+              height={300}
+              src={car?.image_url}
+              alt="car image"
+            />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Cars;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    // domains: [
+    //   "www.alfaromeo.com.au",
+    //   "cdn.carbuzz.com",
+    //   "carsguide-res.cloudinary.com",
+    //   "swadeology.com",
+    //   "spct2000.files.wordpress.com",
+    // ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
