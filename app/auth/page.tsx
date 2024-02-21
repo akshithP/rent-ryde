b5ac534d5f7ca7970c33e09737087bba9c8af758
Akshith-Patil <akshithp13@gmail.com>
2024-02-21 16:57:04 +1100

    Implemented register form design

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Login from "@/components/Auth Form/Login";
+import Register from "@/components/Auth Form/Register";
 
 const page = () => {
   return (
     <div className="flex items-center justify-center bg-gradient-to-b from-secondary to-primary h-screen ">
-      <div className="md:w-96 md:h-[540px] w-80 h-[500px] bg-secondary2 rounded-md px-5 py-5">
-        <Login />
+      <div className="md:w-96 md:h-[560px] w-80 h-[520px] bg-secondary2 rounded-md px-5 py-5">
+        <Login  />
       </div>
     </div>
   );
