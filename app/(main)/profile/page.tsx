a4bf32e7d47f6fb183c06ae1d83f22d082bba0a1
Akshith-Patil <akshithp13@gmail.com>
2024-02-28 13:24:49 +1100

    fixed import error

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/authOptions";
 import { redirect } from "next/navigation";
 
 const Profile = async () => {
